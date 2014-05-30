d10504de35d02d9b2b7327f6dd55f031fe1af8d5
David Keijser <keijser@gmail.com>
2014-05-30 22:20:58 +0200

    retrofit proxy test to neat example file

---
+++ examples/cluster.js
@@ -0,0 +1,48 @@
+var cluster = require('cluster'),
+    logging = require('../lib'),
+    sinks = require('../lib/sinks');
+
+function master() {
+    logging.createProxy();
+
+    logging.getLogger('foo.baz').addSink(sinks.console());
+    logging.getLogger('foo').addSink(sinks.file('./cluster-example.log'));
+
+    for (var i = 0; i < 4; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', function (worker) {
+        logging.getLogger('foo').info('%s has left the building', worker.process.pid);
+    });
+
+    setTimeout(function () {
+        process.exit();
+    }, 10000);
+}
+
+function worker() {
+    var bar = logging.getLogger('foo.bar'),
+        baz = logging.getLogger('foo.baz');
+
+    if (logging.isProxyAvailable()) {
+        logging.enableProxy();
+    }
+
+    process.on('SIGUSR2', function () {});
+
+    setInterval(function () {
+        bar.info('test 1 %s', process.pid);
+        bar.error('test 2 %s', process.pid);
+        baz.debug('test 3 %s', process.pid);
+    }, 1000);
+}
+
+logging.getLogger('foo.bar').level = 40;
+logging.getLogger('foo.baz').level = 10;
+
+if (cluster.isMaster) {
+    master();
+} else {
+    worker();
+}
