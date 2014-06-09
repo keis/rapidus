6f6a8b813c101d0c91bf178b42a2ba1fc4024502
David Keijser <keijser@gmail.com>
2014-06-09 19:07:15 +0200

    extend express example with a access log

--- docs/references.md
+++ docs/references.md
@@ -8,3 +8,6 @@ https://github.com/joyent/node/blob/master/lib/child_process.js
 # python logging
 https://docs.python.org/3/library/logging.html
 http://hg.python.org/cpython/file/3.4/Lib/logging/__init__.py
+
+# morgan the connect logger
+https://github.com/expressjs/morgan
--- examples/express.js
+++ examples/express.js
@@ -15,16 +15,75 @@ logging.addDefaultProcessor(function (record) {
 
 // Configure a logger that writes records to the console with their associated
 // request id
-logging.getLogger('access').addSink(sinks.console(function (record) {
+logging.getLogger('app').addSink(sinks.console(function (record) {
     return record.requestId + ' - ' + record.getMessage();
+}));
+
+// Configure a access log that is more or less directly yanked from morgan the
+// connect logger
+function accessLog() {
+    var logger = logging.getLogger('access');
+
+    function responseTime(req, res) {
+        if (!res._header || !req._startAt) {
+            return '';
+        }
+
+        var tdiff = process.hrtime(req._startAt),
+            ms = tdiff[0] * 1e3 + tdiff[1] * 1e-6;
+
+        return ms.toFixed(3);
+    }
+
+    // A bit of a funky processor that pops the req object that was stored as
+    // `msg` and populates the record with details of the request
+    logger.addProcessor(function (record) {
+        var req = record.msg,
+            res = req.res,
+            url = req.originalUrl || req.url;
+
+        record.msg = record.url = url;
+        record.time = req._startTime;
+        record.remoteAddress = req.connection && req.connection.remoteAddress;
+        record.method = req.method;
+        record.status = res._header ? res.statusCode : null;
+        record.responseTime = responseTime(req, res);
+    });
+
+    // The middleware
+    return function (req, res, next) {
+        req._startAt = process.hrtime();
+        req._startTime = new Date();
+
+        function log() {
+            res.removeListener('finish', log);
+            res.removeListener('close', log);
+            // This abuses the fact that msg is not forced to be a string when
+            // being created and relies on the processor to fix this.
+            // util.format happens to do something pretty useful with objects
+            // passed so this should be a blessed usage.
+            logger.info(req);
+        }
+
+        res.on('finish', log);
+        res.on('close', log);
+
+        next();
+    }
+}
+
+logging.getLogger('access').addSink(sinks.console(function (record) {
+    // Supporting neat tokens like ':method :url :status' would be cool
+    return [record.requestId, record.method, record.url, record.status, '-', record.responseTime].join(' ');
 }));
 
 // Construct a express application with a middleware to configure an id for
 // each request in the cls
 app = express();
 app.use(assignId(namespace));
+app.use(accessLog());
 app.get('/test', function (req, res, next) {
-    var logger = logging.getLogger('access');
+    var logger = logging.getLogger('app');
     logger.info("processing request");
     setTimeout(function () {
         logger.info("request processed");
